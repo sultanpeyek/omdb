f6e48268de416a8013ca8160b7fed435d54ba360
SULTAN PEYEK <sultanpeyek999@gmail.com>
2022-08-31 21:06:45 +0700

    Moving to https

--- src/api/movies.tsx
+++ src/api/movies.tsx
@@ -9,6 +9,6 @@ export type Movie = {
 
 export const getMoviesBySearchValue = (searchValue: string) => {
   return axios.get(
-    `http://www.omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=${searchValue}`,
+    `https://www.omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=${searchValue}`,
   )
 }
