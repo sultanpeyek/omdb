5999df2042fbfea98dda12a45d3c4595f3e16fa9
SULTAN PEYEK <sultanpeyek999@gmail.com>
2022-08-31 20:12:35 +0700

    Adjust eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OMDB_API_KEY: process.env.OMDB_API_KEY,
+  },
   swcMinify: true,
   images: {
     domains: ['localhost:3000', 'm.media-amazon.com', 'img-cdn.magiceden.dev'],
