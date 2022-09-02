4680b35488269f5b7667282ffc228388325ef5c5
SULTAN PEYEK <sultanpeyek999@gmail.com>
2022-09-02 11:06:59 +0700

    Polishing infinite scroll functionality

--- src/components/movies/CardContainer/CardContainer.spec.tsx
+++ src/components/movies/CardContainer/CardContainer.spec.tsx
@@ -1,10 +1,37 @@
+import {WalletAdapterNetwork} from '@solana/wallet-adapter-base'
+import {ConnectionProvider, WalletProvider} from '@solana/wallet-adapter-react'
+import {WalletModalProvider} from '@solana/wallet-adapter-react-ui'
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
 import {render, screen} from '@testing-library/react'
+import {Provider} from 'react-redux'
 
+import store from '@/app/store'
 import CardContainer from '@/components/movies/CardContainer'
+import {RPC_MAINNET_PRIMARY} from '@/constants'
+
+const network = WalletAdapterNetwork.Mainnet
+
+const wallets = [
+  new PhantomWalletAdapter(),
+  new SolflareWalletAdapter({network}),
+]
 
 describe('<CardContainer />', () => {
   it('renders the component', () => {
-    render(<CardContainer>Testing CardContainer</CardContainer>)
+    render(
+      <Provider store={store}>
+        <ConnectionProvider endpoint={RPC_MAINNET_PRIMARY}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <CardContainer>Testing CardContainer</CardContainer>
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </Provider>,
+    )
 
     expect(screen.getByText(/Testing CardContainer/i)).toBeInTheDocument()
   })
--- src/components/movies/CardContainer/CardContainer.tsx
+++ src/components/movies/CardContainer/CardContainer.tsx
@@ -1,6 +1,49 @@
+import {useCallback, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+
+import {fetchMoreMovies} from '@/features/movies/moviesSlice'
+
 const CardContainer = ({children}: any) => {
+  const dispatch = useDispatch()
+
+  const fetchMoreMoviesStatus = useSelector(
+    (state: any) => state.movies.fetchMoreMoviesStatus,
+  )
+  const initialSearchValue = useSelector(
+    (state: any) => state.movies.searchValue,
+  )
+  const pageNumber = useSelector((state: any) => state.movies.pageNumber)
+  const totalResults = useSelector((state: any) => state.movies.totalResults)
+
+  const isBottom = (el: any) => {
+    return el.getBoundingClientRect().bottom <= window.innerHeight
+  }
+
+  const trackScrolling = useCallback(() => {
+    const el = document.getElementById('card-container')
+    if (isBottom(el) && fetchMoreMoviesStatus !== 'loading') {
+      dispatch(
+        fetchMoreMovies({
+          searchValue: initialSearchValue,
+          pageNumber: pageNumber + 1,
+        }),
+      )
+    }
+  }, [fetchMoreMoviesStatus, dispatch, pageNumber, initialSearchValue])
+
+  useEffect(() => {
+    pageNumber < totalResults / 10 &&
+      document.addEventListener('scroll', trackScrolling)
+    return () => {
+      document.removeEventListener('scroll', trackScrolling)
+    }
+  }, [trackScrolling, dispatch, pageNumber, totalResults])
+
   return (
-    <div className="container grid grid-cols-2 gap-4 py-4 md:py-8 md:gap-8 md:grid-cols-3 max-w-[960px]">
+    <div
+      id="card-container"
+      className="container grid grid-cols-2 gap-4 py-4 md:py-8 md:gap-8 md:grid-cols-3 max-w-[960px]"
+    >
       {children}
     </div>
   )
